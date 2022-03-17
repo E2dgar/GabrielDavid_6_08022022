6a4a27b45d105f9cb4655632126f7edac159a6a0
DG <gdavidcefim@gmail.com>
2022-03-17 15:07:14 +0100

    Webpack path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-	publicPath: '/GabrielDavid_6_08022022/dist/'
+		publicPath: '/GabrielDavid_6_08022022/dist/'
   },
   experiments: {
     topLevelAwait: true
